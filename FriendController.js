71f7f5dabc31465d4360cefc5892b39204cccf4d
Adam Winn <adambwinn@gmail.com>
2014-04-16 17:58:54 -0600

    add filtering

--- FriendController.js
+++ FriendController.js
@@ -1,15 +1,12 @@
 
 
-angular.module("angular-friends").controller('FriendController', function($scope){
+angular.module("angular-friends").controller('FriendController', function($scope, $http){
         //$scope.test = "hello world";
+    $http({method: "get", url: "https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json"}).success(function(data){
+        $scope.friends = data.results;
+    })
 
-        $.getJSON("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json",function(data){
-            $scope.$apply(function(){        //  need to call apply so angular knows to apply this stuff to view
-                $scope.friends = data.results;
-            });
-
-
-        } )
+    $scope.searchterm;
 
 })
 
