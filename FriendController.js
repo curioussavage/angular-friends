91fcb68f8af71f2964506aa59b744dff45928af8
Adam Winn <adambwinn@gmail.com>
2014-04-15 20:53:44 -0600

    add basic app, jquery to get friends list, repeat each friend

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,18 @@
 
 
+angular.module("angular-friends").controller('FriendController', function($scope){
+        //$scope.test = "hello world";
+
+        $.getJSON("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json",function(data){
+            $scope.$apply(function(){        //  need to call apply so angular knows to apply this stuff to view
+                $scope.friends = data.results;
+            });
+
+
+        } )
+
+})
+
 
 
 
---
+++ app.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+    var myapp = angular.module('angular-friends',[]);
+
+})();
+
+
+// the anonymous function is wrapped so that it can be called on line four with the empty parentheses.
+
+
+
+
